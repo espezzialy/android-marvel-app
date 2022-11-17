0531401c2e074e8c2aee0d60278916a5689903c8
Espezzialy Souza <espezzialy@outlook.com>
2022-11-17 15:24:15 -0300

    adding the character presentation and configuring the recycler view

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,20 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.espezzialy.core.domain.model.Character
 import com.example.marvelapp.R
+import com.example.marvelapp.databinding.FragmentCharactersBinding
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class CharactersFragment : Fragment() {
+
+    private var _binding : FragmentCharactersBinding? = null
+    private val binding : FragmentCharactersBinding get() = _binding!!
+
+    private val charactersAdapter : CharactersAdapter = CharactersAdapter()
 
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    ) = FragmentCharactersBinding.inflate(
+        inflater,
+        container,
+        false
+    ).apply {
+        _binding = this
+    }.root
 
-        return inflater.inflate(R.layout.fragment_characters, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initCharactersAdapter()
+
+        charactersAdapter.submitList(
+            listOf(
+                Character("Spider Man 1", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("Spider Man 2", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("Spider Man 3", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("Spider Man 4", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+                Character("Spider Man 5", "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"),
+            )
+        )
+
     }
 
+    private fun initCharactersAdapter() {
+        binding.recyclerCharacters.run {
+            setHasFixedSize(true)
+            adapter = charactersAdapter
+        }
+
+    }
 
 }
--- core/src/main/java/com/espezzialy/core/data/network/response/ThumbnailResponse.kt
+++
@@ -1,8 +0,0 @@
-package com.espezzialy.core.data.network.response
-
-import com.google.gson.annotations.SerializedName
-
-data class ThumbnailResponse(
-    val path: String,
-    val extension: String
-)
